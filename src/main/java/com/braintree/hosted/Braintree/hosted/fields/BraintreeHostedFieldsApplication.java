23efd57fdf1d893bf35832331599627ddf218693
wpittman <wpittman@paypal.com>
2019-01-23 10:15:24 -0600

    initial commit

---
+++ src/main/java/com/braintree/hosted/Braintree/hosted/fields/BraintreeHostedFieldsApplication.java
@@ -0,0 +1,14 @@
+package com.braintree.hosted.Braintree.hosted.fields;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraintreeHostedFieldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraintreeHostedFieldsApplication.class, args);
+	}
+
+}
+
